1c9cd2b0c65b104d6ed4ad0a3880241c248a6a9e
paulaborregoon <paula.borregon@estudiantat.upc.edu>
2025-12-17 09:21:26 +0100

    arreglosdosh

--- Assets/Scripts/WorldManager.cs
+++ Assets/Scripts/WorldManager.cs
@@ -1,32 +1,23 @@
 using UnityEngine;
-
 public class WorldManager : MonoBehaviour
 {
-    // Valor único del estado (0 a 1)
     public float estadoGlobal = 0f;
-
-    // Referencia estática para acceder fácilmente
     public static WorldManager Instance;
 
     void Awake()
     {
         if (Instance == null)
-        {
             Instance = this;
-        }
+        else
+            Destroy(gameObject);
     }
 
-    // Método para cambiar el estado
     public void CambiarEstado(float cantidad)
     {
         estadoGlobal += cantidad;
-
-        // Limitar entre 0 y 1
-        if (estadoGlobal > 1f) estadoGlobal = 1f;
-        if (estadoGlobal < 0f) estadoGlobal = 0f;
+        estadoGlobal = Mathf.Clamp(estadoGlobal, -100f, 100f);
     }
 
-    // Método para obtener estado actual
     public float GetEstadoActual()
     {
         return estadoGlobal;
